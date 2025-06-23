9cf8d95d8dc25587a9045836e0f0f077158a6cf7
iain-armitage <iainarmitage1@gmail.com>
2025-06-23 12:26:39 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
-import Sidebar from './Sidebar';
-import Topbar from './Topbar';
+import Layout from '../components/Layout';
 
-export default function Layout({ children }) {
+export default function Home() {
   return (
-    <div className="flex h-screen bg-gray-100">
-      <Sidebar />
-      <div className="flex flex-col flex-1">
-        <Topbar />
-        <main className="p-6 overflow-y-auto">{children}</main>
+    <Layout>
+      <div className="p-4">
+        <h1 className="text-2xl font-bold">Welcome to TradeX Dashboard</h1>
+        <p>This is the starting point of your Next.js + Tailwind app.</p>
       </div>
-    </div>
+    </Layout>
   );
 }
