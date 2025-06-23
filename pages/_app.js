12578c6ed355e822102c3b5973dc51deb74e22bc
iain-armitage <iainarmitage1@gmail.com>
2025-06-23 16:32:33 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/globals.css';
+// pages/_app.js
+import Layout from "../components/Layout";
+import "../styles/globals.css";
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
+
+export default MyApp;
