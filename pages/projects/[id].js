5bbebd6c11401db9eef0775b68ad17fa0b374d43
iain-armitage <iainarmitage1@gmail.com>
2025-06-30 17:20:09 +1000

    Add files via upload

---
+++ pages/projects/[id].js
@@ -0,0 +1,4 @@
+// Project Detail Page (to be implemented)
+export default function ProjectDetail() {
+  return <div>Project Detail View</div>;
+}
---
+++ pages/quotes/[id].js
@@ -0,0 +1,4 @@
+// Quote Detail Page (to be implemented)
+export default function QuoteDetail() {
+  return <div>Quote Detail View</div>;
+}
